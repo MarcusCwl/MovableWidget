f26e773f6ab44cf6b5859b323064fcfc9b7d89bb
Bao Chen <bao.chen@grabtaxi.com>
2020-02-26 17:43:24 +0800

    Fix pub spec

--- movable_widget/lib/MovableWidget.dart
+++ movable_widget/lib/MovableWidget.dart
@@ -4,44 +4,81 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-/// A Calculator.
 class MovableWidget extends StatefulWidget {
+  final double top;
+  final double left;
+  final double right;
+  final double bottom;
+
+  //Key of Widget which need to show and impl movable statement
+  //We use this to get size of widget
+  final GlobalKey widgetKey;
+  //Widget which need to show and impl movable statement
+  final Widget widget;
+
+  MovableWidget(this.left, this.top, this.right, this.bottom, this.widget,
+      this.widgetKey);
+
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return MovableState();
+    return MovableState(left, top, right, bottom, widget, widgetKey);
+  }
+}
+
+class MovableState extends State<MovableWidget>
+    with SingleTickerProviderStateMixin {
+  final double xMin;
+  final double yMin;
+  final double xMax;
+  final double yMax;
+  final GlobalKey widgetKey;
+  Widget child;
+
+  double top;
+  double left;
+
+  MovableState(
+      this.xMin, this.yMin, this.xMax, this.yMax, this.child, this.widgetKey) {
+    top = this.yMin;
+    left = this.xMin;
   }
 
-
-}
-class MovableState extends State<MovableWidget> with SingleTickerProviderStateMixin {
-  double _top = 0.0; //距顶部的偏移
-  double _left = 0.0;//距左边的偏移
+  Size getSize() {
+    final RenderBox renderBoxRed = widgetKey.currentContext.findRenderObject();
+    return renderBoxRed.size;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
         Positioned(
-          top: _top,
-          left: _left,
+          top: top,
+          left: left,
           child: GestureDetector(
-            child: CircleAvatar(child: Text("A")),
-            //手指按下时会触发此回调
-            onPanDown: (DragDownDetails e) {
-              //打印手指按下的位置(相对于屏幕)
-              print("用户手指按下：${e.globalPosition}");
-            },
-            //手指滑动时会触发此回调
+            child: child,
             onPanUpdate: (DragUpdateDetails e) {
-              //用户手指滑动时，更新偏移，重新构建
               setState(() {
-                _left += e.delta.dx;
-                _top += e.delta.dy;
+                double tempLeft = left + e.delta.dx;
+                double tempTop = top + e.delta.dy;
+                if (tempLeft >= xMax - getSize().width) {
+                  left = xMax - getSize().width;
+                } else if (tempLeft <= xMin) {
+                  left = xMin;
+                } else {
+                  left = tempLeft;
+                }
+
+                if (tempTop >= yMax - getSize().height) {
+                  top = yMax - getSize().height;
+                } else if (tempTop <= yMin) {
+                  top = yMin;
+                } else {
+                  top = tempTop;
+                }
               });
             },
-            onPanEnd: (DragEndDetails e){
-              //打印滑动结束时在x、y轴上的速度
+            onPanEnd: (DragEndDetails e) {
               print(e.velocity);
             },
           ),
